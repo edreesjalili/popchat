0bb4ce067aca3be2a1b9f88632027f4d0c6573cf
Hamza Ahmed <hahmed149@outlook.com>
2018-07-22 01:11:13 -0500

    updated conversation list to a tuple

--- src/app/conversation-list/conversation-list.component.ts
+++ src/app/conversation-list/conversation-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/shared/auth.service';
 import { ConversationService } from '../conversation/shared/conversation.service';
 import { Conversation } from '../conversation/shared/conversation';
+import { User } from '../user/shared/user';
+import { UserService } from '../user/shared/user.service';
 
 @Component({
   selector: 'app-conversation-list',
@@ -11,17 +13,32 @@ import { Conversation } from '../conversation/shared/conversation';
 export class ConversationListComponent implements OnInit {
 
   conversations : Conversation[];
+  combos = [];
+
 
   constructor(
     public authService: AuthService,
+    private userService: UserService,
     private conversationService: ConversationService
+
   ) { }
 
   ngOnInit() {
     this.conversationService.getConversationsForUser( this.authService.currentUser._id).subscribe((conversations: Conversation[]) =>
     {
-      this.conversations = conversations;
+      for(let conversation of conversations )
+      {
+        const pulledUserId = this.conversationService.getOtherUserId(conversation.userIds, this.authService.currentUser._id);
+        this.userService.getUser(pulledUserId).subscribe(user =>
+          {
+            this.combos.push({conversation,user})
+          })
+      }
+
+
     } );
+
+  
   }
 
 }
